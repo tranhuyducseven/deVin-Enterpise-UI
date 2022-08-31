451065d7e9dd23eac88e795a50f4e70a03b3255f
tranhuyducseven <tranhuyducseven@gmail.com>
2022-08-31 23:15:43 +0700

    feature(home+overview): add animation box

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Image } from "semantic-ui-react";
 import { MainLayout } from "../../layouts/MainLayout";
 import { SubstrateContextProvider } from "../../substrate-lib";
+import AnimationBox from "../components/AnimationBox";
 
 const HomeScreen = () => {
   return (
@@ -11,7 +12,8 @@ const HomeScreen = () => {
         <div className="bg-black h-screen flex justify-center items-center">
           <Link
             to="/app"
-            className="bg-[#ffdd50] button-connect w-[40rem] hover:scale-105 active:scale-90 duration-300 border-8 border-white p-8 rounded-lg"
+            className="bg-[#ffdd50] button-connect w-[40rem] hover:scale-105 active:scale-90 duration-300 border-8 border-white p-8 rounded-lg animate-pulse cursor-pointer z-10"
+            style={{ animationDuration: "4s" }}
           >
             <Image src={`${process.env.PUBLIC_URL}/assets/deVin.png`}></Image>
             <div className="flex flex-row items-center justify-center px-4 py-1  rounded-lg mt-5">
@@ -20,6 +22,7 @@ const HomeScreen = () => {
               </span>
             </div>
           </Link>
+          <AnimationBox />
         </div>
       </MainLayout>
     </SubstrateContextProvider>
--- src/screens/organizations/index.jsx
+++ src/screens/organizations/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Dashboard from "./components/Dashboard";
 import { DefaultLayout } from "../../layouts/DefaultLayout";
 import { SubstrateContextProvider } from "../../substrate-lib";
+import AnimationBox from "../components/AnimationBox";
 const OrganizationScreen = ({props}) => {
   return (
     <SubstrateContextProvider>
       <DefaultLayout>
         <Dashboard {...props} />
+        <AnimationBox />
       </DefaultLayout>
     </SubstrateContextProvider>
   );
